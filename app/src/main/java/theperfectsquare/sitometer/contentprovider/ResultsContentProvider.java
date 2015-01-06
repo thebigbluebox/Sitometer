3d937fd67961d6a9827ad72b4769d31f95fdf150
thebigbluebox <tong_901@hotmail.com>
2015-01-05 22:28:47 -0500

    Inital Commit

---
+++ app/src/main/java/theperfectsquare/sitometer/contentprovider/ResultsContentProvider.java
@@ -0,0 +1,7 @@
+package theperfectsquare.sitometer.contentprovider;
+
+/**
+ * Created by tai on 05/01/15.
+ */
+public class ResultsContentProvider {
+}
---
+++ app/src/main/java/theperfectsquare/sitometer/database/ResultsTable.java
@@ -0,0 +1,7 @@
+package theperfectsquare.sitometer.database;
+
+/**
+ * Created by tai on 05/01/15.
+ */
+public class ResultsTable {
+}
